75474c31429ea588afb4ea90063a37d23958ae95
rahaf fallatah <rahaf77553@gmail.com>
2025-02-09 04:09:50 +0300

    lb 12

---
+++ lab12-.py
@@ -0,0 +1,12 @@
+movies = [
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+for i in movies:
+    for i in range(0,7):
+        avreg = movies[0]+movies[1]+movies[2]+movies[3]+movies[4]+movies[5]/6
+print(avreg)
